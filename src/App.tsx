429c382cb36cb2e0d60dcf6c1ae7ebca36011176
DayYoung <machuanjia@laiye.com>
2021-10-19 15:40:36 +0800

    chore: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import logo from '@/assets/logo.svg';
-import styles from '@/App.module.less';
-import { Button } from 'antd';
+/*
+ * @Author: D.Y.M
+ * @Date: 2021-10-19 14:45:00
+ * @LastEditTime: 2021-10-19 15:39:48
+ * @FilePath: /beaver/src/App.tsx
+ * @Description:
+ */
+import React from 'react'
+
+import { Button } from 'antd'
+
+import styles from '@/App.module.less'
+import logo from '@/assets/logo.svg'
 
 function App() {
   return (
     <div className={styles.App}>
       <header className="App-header bg-red-400">
         <Button>sdsd</Button>
-        
+
         <img src={logo} className="App-logo border-8" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -23,7 +32,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,26 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-10-19 10:19:26
- * @LastEditTime: 2021-10-19 15:02:37
+ * @LastEditTime: 2021-10-19 15:39:53
  * @FilePath: /beaver/src/index.tsx
- * @Description: 
+ * @Description:
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 import '@/styles/index.less'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
